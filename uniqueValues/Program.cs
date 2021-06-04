94dd6bf5782251257de89d5918565263ba9d931e
lopataA <lopata.ad@gmail.com>
2021-06-04 19:08:36 +0300

    'asdads'

--- uniqueValues/Program.cs
+++ uniqueValues/Program.cs
@@ -6,75 +6,69 @@ namespace uniqueValues
 {
     class Program
     {
-        public static int[] uniqueNumList(int [] a,int[]b)
-        {   var o = uniqueFromFirstArr(a, b);
-            var s = uniqueFromFirstArr(b, a);
-            var r = toConnect(o, s);          
-            return r;
-        }
-        public static int[] toConnect(int[] x, int[] y) 
-        {
+        
+        public static int[] toConnect(int[] x, int[] y){
             int counter = 0;
             int[] r = new int[x.Length+y.Length];
             for (int i = 0; i < x.Length; i++)
             {
                 r[counter++] = x[i];
             }
-            for (int k = 0; k< y.Length; k++)
+            for (int i = 0; i< y.Length; i++)
             {
-                r[counter++] = y[k];
+                r[counter++] = y[i];
             }
              return r;
         }
-        public static bool  isExist(int[]b,int x)
-        {   bool isExist = false;
-            for (int k = 0; k < b.Length; k++)
-            {
-             int   y = b[k];
-                if (x == y)
-                {
-                    isExist = true;
-                    break;
-                }
-            }
-            return isExist;
+        public static bool  isExist(int[]b,int x){
+           
+            for (int k = 0; k < b.Length; k++){
+                 int   y = b[k];
+                if (x == y) return true;
+               }
+            return false;
         }
        
-        public static int[] uniqueFromFirstArr(int[] a, int[] b)
-        {
-            int c1 = 0;
-            int counter = 0;
+        public static int[] uniqueFromFirstArr(int[] a, int[] b){          
             int?[] u = new int?[a.Length];
-            
-            for (int i = 0; i < a.Length; i++)
-            {
+
+            int counter = 0;
+            for (int i = 0; i < a.Length; i++){
                 int x = a[i];
-                if (isExist(b, x) == false)
-                {
-                    u[counter++] = (x);
+                if (isExist(b, x) == false){
+                    u[counter++] = x;
                 }
             }
+
+
             int[] p = new int[counter];
+            int count1 = 0;
             for (int i = 0; i <counter; i++)
             {
-                if (u[i]!= null)
+                if (u[i] != null) 
                 {
-                    p[c1++] = u[i].Value;
+                    p[count1++] = u[i].Value;
                 }
             }
             return p;
         }
-        static void Main(string[] args)
-        {   //int[] arr_first = { 1, 2, 3 ,4,9,10};
+        public static int[] uniqueNumList(int[] a, int[] b)
+        {
+            var o = uniqueFromFirstArr(a, b);
+            var s = uniqueFromFirstArr(b, a);
+            var r = toConnect(o, s);
+            return r;
+        }
+        static void Main(string[] args){
+            //int[] arr_first = { 1, 2, 3 ,4,9,10};
             //  int[] arr_first = { 1, 2, 3, 4, 9, -2 };
             //int[] arr_first = { 1 };
-              int[] arr_first = { 0,0,0};
-          //  int[] arr_first = { 1, 2, 3,5,9,10 };
+            //  int[] arr_first = { 0,0,0};
+           int[] arr_first = { 1, 2, 3,5,9,10 };
             int[] arr_second = { 1, 5, 3,6};
           var r=uniqueNumList(arr_first, arr_second);
 
-            foreach (int item in r)
-            {
+            foreach (int item in r){
                 Console.WriteLine(item);
             }
         }
