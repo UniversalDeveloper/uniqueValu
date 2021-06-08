f24da7c180f96e500f87c16cd4cb50ef19368535
lopataA <lopata.ad@gmail.com>
2021-06-08 12:51:27 +0300

    Fixed code.Formated.Added tests methods in a cod of program.

--- uniqueValues/Program.cs
+++ uniqueValues/Program.cs
@@ -5,85 +5,61 @@ using System.Linq;
 namespace UniqueValues
 {
    public class  Program
-    {    public static int[] UniqNumbers(int[] a, int[] b){
-            //  DuplicatClean(a);
-            var test= DuplicatNast(a);
-            var test1 = DuplicatNast(b);
-                     
+    {   public static int[] UniqNumbers(int[] a, int[] b){           
+            var unieFromArrFirst = WithouDuplInArr(a);
+            var unieFromArrSecond = WithouDuplInArr(b);                     
             int?[] uniqNum = new int?[10];
             int counter = 0;
-            for (int i = 0; i < test.Length; i++){              
-                if (IsExist(test1, test[i]) == false){
-                    uniqNum[counter++] = test[i];
+            for (int i = 0; i < unieFromArrFirst .Length; i++){              
+                if (IsExist(unieFromArrSecond, unieFromArrFirst[i]) == false){
+                    uniqNum[counter++] = unieFromArrFirst[i];
                 }
             }
             int[] clearArr = CleanNull(uniqNum, counter);
             return clearArr;
         }
-
-        public static int CountAnUniq(int[] a)
-        {
+        public static int CountAnUniq(int[] a){
             int notUniqueElcount = 0;
-            for (int i = 1; i <a.Length; i++)
-            {
-                for (int k = i-1; k >=0; k--)
-                { if (a[i] == a[k])
+            for (int i = 1; i <a.Length; i++){
+                for (int k = i-1; k >=0; k--){ 
+                    if (a[i] == a[k])
                         notUniqueElcount++;
-                    break;
-
+                          break;
                 }
             }
             return notUniqueElcount;
         }
-      
-
-            public static int[] DuplicatNast(int[] a) {
-
+        public static int[] WithouDuplInArr(int[] a){
             int?[] arrVithNull = new int?[a.Length];
-            int buf = 0;
+            int flag = 0;
             int[] cleanArr = new int[a.Length];
             int counter = 0;
             bool isExist = false;
-           bool isExist2 = false;
-            for (int i = 0; i < a.Length; i++) {
-                isExist = false;
-               
-                for (int k = 0; k < a.Length; k++) {
-
+            bool isExist2 = false;
+            for (int i = 0; i < a.Length; i++){
+                isExist = false;               
+                for (int k = 0; k < a.Length; k++){
                     if (i == k) continue;
-                    if (a[i] == a[k]) {
+                    if (a[i] == a[k]){
                         isExist = true;
-                        buf = 0;
+                        flag = 0;
                         break;
                     }
-                    if (isExist == false) {
-                        buf = a[i];
+                    if (isExist == false){
+                        flag = a[i];
                     }
-                    if (isExist2 == true) {
+                    if (isExist2 == true){
                         isExist2 = false;
                         break;
-                    }
-                  
+                    }                  
                 } 
-
-                if (isExist == false) {
-                    arrVithNull[counter++] = buf; 
-                   
+                if (isExist == false){
+                    arrVithNull[counter++] = flag;                    
                 }
             }
-
-            int[] clearArr = CleanNull(arrVithNull, counter);
-            foreach (int item in clearArr)
-            {
-
-                Console.Write(item);
-                Console.WriteLine();
-            }
+            int[] clearArr = CleanNull(arrVithNull, counter);         
             return clearArr;
-
-
-        }
-        
+        }      
         public static bool IsExist(int[] b, int x){
             for (int i = 0; i < b.Length; i++){               
                 if (x == b[i]) return true;
@@ -122,41 +98,36 @@ namespace UniqueValues
             foreach (int item in unicNum){
                 Console.Write(item);
             }
+            Console.WriteLine();
         }
        public static void UniqueValueTests(){           
-            int[] arrC = { 1, 2,3,5,9,7,-10,0,11,1};
-            int[] arrD = { 1, 2, 3, 5, 9, 7, -10, 0, 11, 189 };
-            int[] expected1 = { 189 };
-            int[] actual1 = Program.UniqEndList(arrC, arrD);
+            int[] arrA =  { 1, 1, 3 ,1 };
+            int[] arrB = { 9, 5, 9 };
+            int[] expected1 = { 3, 5 };
+            int[] actual1 = Program.UniqEndList(arrA, arrB);
             if (expected1.SequenceEqual(actual1)){
-                Console.WriteLine("True LongArr and NegativeNumber");
+                Console.WriteLine("True: shows unique values of two arrays");
             }
             else{
-                Console.WriteLine("False LongArr and NegativeNumber");
+                Console.WriteLine("False: dose not shows unique values of two arrays");
             }
-
-            int[] arrA = { 0,0,0};
-            int[] arrB = { 1, 2, 3,0 };
-            int[] expected = {1,2,3 };
-            int[] actual = Program.UniqEndList(arrA, arrB);
-            if (expected.SequenceEqual(actual))
+            int[] arrC = { 1, 4, -2, 8 ,7, 159,0 };
+            int[] arrD = { 9, 5, 9 };
+            int[] expected2 = { 1, 4, -2, 8, 7, 159 ,5};
+            int[] actual3 = Program.UniqEndList(arrC, arrD);
+            if (expected2.SequenceEqual(actual3))
             {
-                Console.WriteLine("True with '0'");
+                Console.WriteLine("True: shows unique values with negative and long arrey");
             }
-            else
-            {
-                Console.WriteLine("False LongArr and NegativeNumber");
-            }
-
+            else{
+                Console.WriteLine("False: dose not shows unique values with negative and long arrey");
+            }           
         }
         static void Main(string[] args){
-            // int[] arrA = new int[6];
-             int[] arrA = {1,  4, 4};
-          //  int[] arrA = { 1, 1, 3 ,1};
-          // int[] arrA = { 1, 2,3,1 };
-            int[] arrB = {9,5,9};
-            ShowResult(arrA, arrB);
-          //  UniqueValueTests();
+           int[] arrA = { 1, 1, 3 ,1};
+           int[] arrB = {9,5,9};
+           ShowResult(arrA, arrB);
+           UniqueValueTests();
         }
     }
 }
